649b8a171882ad94c4869973e40c98010832e4ad
Samin Thapa <saminthapa88@gmail.com>
2022-06-13 07:38:44 +0545

    z

--- lib/main_screen/attendance/sync_attendance.dart
+++ lib/main_screen/attendance/sync_attendance.dart
@@ -50,24 +50,15 @@ class SyncAttendance extends StatelessWidget {
 }
 
 Future<AttendanceModel> fetchAttendance() async {
-  AttendanceModel fetchedAttendance = AttendanceModel(nepaliDate: 'nepaliDate',
-      englishDate: 'englishDate',
-      attendDateTime: 'attendDateTime',
-      latitude: 'latitude',
-      longitude: 'longitude',
-      deviceId: 'deviceId',
-      networkId: 'networkId',
-      altitude: 'altitude');
   final response = await http
       .get(Uri.parse('http://api.ssgroupm.com/Api/Attendence/GetAttendence'));
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return fetchedAttendance.fromJson(jsonDecode(response.body));
+
+    AttendanceModel fetchedAttendance = AttendanceModel.fromJson(jsonDecode(response.body));
+    return fetchedAttendance;
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
+
     throw Exception('Failed to load attendance log');
   }
 }
